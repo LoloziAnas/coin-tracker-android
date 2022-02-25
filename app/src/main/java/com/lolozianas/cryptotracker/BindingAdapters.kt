24ed82431e54535516210a358c43e7273ab31b1d
LoloziAnas <lolozianas1@gmail.com>
2022-02-25 20:06:13 +0000

    implementing Binding adapters

---
+++ app/src/main/java/com/lolozianas/cryptotracker/BindingAdapters.kt
@@ -0,0 +1,41 @@
+package com.lolozianas.cryptotracker
+
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.lolozianas.cryptotracker.network.Coin
+import com.lolozianas.cryptotracker.ui.overview.APIStatus
+import com.lolozianas.cryptotracker.ui.overview.OverviewAdapter
+
+/**
+ * Updates the data shown in the [RecyclerView].
+ */
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<Coin>?) {
+    val adapter = recyclerView.adapter as OverviewAdapter
+    adapter.submitList(data)
+}
+
+/**
+ * This binding adapter displays the [] of the network request in an image view.  When
+ * the request is loading, it displays a loading_animation.  If the request has an error, it
+ * displays a broken image to reflect the connection error.  When the request is finished, it
+ * hides the image view.
+ */
+@BindingAdapter("apiStatus")
+fun bindStatus(statusImageView: ImageView, status: APIStatus?) {
+    when (status) {
+        APIStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        APIStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        APIStatus.SUCCESS -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
+}
